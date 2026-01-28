1ec1ce50a3a4a6d74ed23307bc35bb7186f4592f
huuthinh-btc <thinhbomong0708@gmail.com>
2026-01-29 06:02:43 +0700

    fix admin login redirect vite

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -5,7 +5,7 @@ import { toast } from "react-toastify";
 export const DoctorContext = createContext();
 
 const DoctorContextProvider = (props) => {
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = "https://hms-fullstack.onrender.com";
   const [dToken, setDToken] = useState(
     localStorage.getItem("dToken") ? localStorage.getItem("dToken") : ""
   );
